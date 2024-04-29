814dbd681ba118bd83ccf27d9ba1cc5b26056956
JOe Molina <joedodaniljr123@gmail.com>
2024-04-29 16:21:50 -0400

    fade-in vista fixed

--- src/app/vista/img.tsx
+++ src/app/vista/img.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
+import './vista.css'
 
 //@ts-ignore
 const ImageOrVideo = ({ currentImage }: any) => {
 
     console.log(currentImage)
+
+    const fadeInStyle = {
+        transition: 'opacity 0.5s ease-in-out',  // Animation properties
+
+    };
 
     if (currentImage.type === "img") {
         return (
@@ -13,6 +19,7 @@ const ImageOrVideo = ({ currentImage }: any) => {
                 alt="Image"
                 className='h-full mx-auto fade-in'
                 key={currentImage.name} // Asigna una key única
+
             />
         );
     } else {
