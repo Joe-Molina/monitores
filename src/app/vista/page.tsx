7e2eb781cfbe523b612f36b834ff59522357f4d2
JOe Molina <joedodaniljr123@gmail.com>
2024-04-30 10:54:04 -0400

    duration error fixed

--- src/app/vista/page.tsx
+++ src/app/vista/page.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import Imga from './Imagen'
 import { prisma } from '../../libs/prisma'
+import { verificarEstadoActividad } from '../services/verificarActividad'
+import Image from 'next/image'
 
 async function Vista() {
 
     const publicidades = await prisma.publicidad.findMany()
 
+    const ActivePublis = publicidades.filter(publi => verificarEstadoActividad(publi.fecha_inicio, publi.Fecha_Fin) === true)
+
+    console.log(publicidades)
+    console.log(ActivePublis)
+
 
     return (
         <div className='bg-black w-screen h-screen' >
-            <Imga
-                //@ts-ignore
-                data={publicidades} />
+
+            {ActivePublis.length > 0 && <Imga data={ActivePublis} />}
+            {ActivePublis.length === 0 && <div className='relative max-w-[800px] h-full mx-auto'><Image src='/iconos/update.svg' layout='fill' alt='gotito' /></div>}
+
         </div>
     )
 }
