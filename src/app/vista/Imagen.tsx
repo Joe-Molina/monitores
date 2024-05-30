1e892695d4c25bc1bb054ce5c0d81d1da686b3e4
jodomodev <joedodaniljr123ggmail.com>
2024-05-30 09:59:48 -0400

    new color positionview

--- src/app/vista/Imagen.tsx
+++ src/app/vista/Imagen.tsx
@@ -108,7 +108,7 @@ const ImageRotator = ({ data }: any) => {
 
         return (
             <div>
-                <div className="flex items-center justify-center z-30 absolute border border-white w-10 h-7 rounded-sm top-5 right-3 opacity-70 bg-slate-50">{currentImageIndex + 1}/{total}</div>
+                <div className="flex items-center justify-center z-30 absolute border border-white w-10 h-7 rounded-sm top-5 right-3 opacity-70 bg-slate-50 text-neutral-950">{currentImageIndex + 1}/{total}</div>
             </div>
         )
     }
